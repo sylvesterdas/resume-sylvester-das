a92e8337e52732b4295aaf0f0b0303271db4f000
Sylvester Das <sylvester.das@gmail.com>
2021-09-22 21:52:19 +0530

    Custom seo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { faFacebook, faTwitter, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { Title, Meta } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   faFacebook = faFacebook
   faInstagram = faInstagram
   faTwitter = faTwitter
   faLinkedinIn = faLinkedinIn;
 
-  constructor() { }
-
-  ngOnInit(): void {
-
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+  ) {
+    const { title, meta } = environment;
+    this.titleService.setTitle(title);
+    this.metaService.addTags(meta);
   }
 
 }
