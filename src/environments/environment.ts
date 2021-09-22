c0e26a5b54c7a2f926419b70d8ade417957f6290
Sylvester Das <sylvester.das@gmail.com>
2021-09-22 21:32:46 +0530

    env for title and meta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  title: "Sylvester Das - Software programmer, Website and Application developer",
+  meta: [
+    { name: 'author', content: 'Sylvester Das' },
+    { name: 'description', content: 'My name is Sylvester Das, I have been working as a MEAN / MERN stack developer. I have a love of performance and object oriented programming, and I have lots of experience in the production of modern web and mobile applications with high performance. I loving creating awesome products and as per my clients’ need. I think user experience when I try to craft something for my clients. Making it awesome.' },
+    { name: 'keywords', content: 'Software developer in mumbai, web application developer in mumbai, hire website developer in mumbai, Sylvester Das, mobile app developer in mumbai, hybrid app developer in mumbai, developer in mumbai, programmer in mumbai' },
+    { name: 'og:image', content: 'assets/images/og-photo.avif' },
+    { name: 'og:image:type', content: 'image/avif' },
+    { name: 'og:image:width', content: '1024' },
+    { name: 'og:image:height', content: '1024' },
+    { name: 'google-site-verification', content: 'hQ0SYSOhJPTThGQDfYW9XvZIlJB6X2gs-K1nUny2PFA' },
+    { name: 'p:domain_verify', content: '6a4ac6b3b4f9c9ef78bd2a2f0e90c42e' },
+  ]
 };
 
 /*
