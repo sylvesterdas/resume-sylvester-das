4e26601f6d1a4bf0b853ff9681ee37e7b0ea4332
Sylvester Das <sylvester.das@gmail.com>
2021-09-24 12:23:25 +0530

    deployment fixed

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,5 @@ const functions = require('firebase-functions');
 const {app} = require(`${process.cwd()}/dist/server/main.js`);
 
 exports.app = functions
-  .region('asia-east2')
   .runWith({ memory: '128MB', maxInstances: 2, minInstances: 1 })
   .https.onRequest(app());
