585f10ab14d032be803209c0d53a910c1589e63d
Sylvester Das <sylvester.das@gmail.com>
2021-09-25 19:47:09 +0530

    meta added for twitter

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,11 +5,19 @@ export const environment = {
     { name: 'author', content: 'Sylvester Das' },
     { name: 'description', content: 'My name is Sylvester Das, I have been working as a MEAN / MERN stack developer. I have a love of performance and object oriented programming, and I have lots of experience in the production of modern web and mobile applications with high performance. I loving creating awesome products and as per my clients’ need. I think user experience when I try to craft something for my clients. Making it awesome.' },
     { name: 'keywords', content: 'Software developer in mumbai, web application developer in mumbai, hire website developer in mumbai, Sylvester Das, mobile app developer in mumbai, hybrid app developer in mumbai, developer in mumbai, programmer in mumbai' },
+    { name: 'og:title', content: 'Sylvester Das - Software programmer, Website and Application developer' },
+    { name: 'og:url', content: 'https://www.sylvesterdas.com' },
+    { name: 'og:description', content: 'My name is Sylvester Das, I have been working as a MEAN / MERN stack developer. I have a love of performance and object oriented programming, and I have lots of experience in the production of modern web and mobile applications with high performance. I loving creating awesome products and as per my clients’ need. I think user experience when I try to craft something for my clients. Making it awesome.' },
     { name: 'og:image', content: 'assets/images/og-photo.avif' },
     { name: 'og:image:type', content: 'image/avif' },
     { name: 'og:image:width', content: '1024' },
     { name: 'og:image:height', content: '1024' },
     { name: 'google-site-verification', content: 'hQ0SYSOhJPTThGQDfYW9XvZIlJB6X2gs-K1nUny2PFA' },
     { name: 'p:domain_verify', content: '6a4ac6b3b4f9c9ef78bd2a2f0e90c42e' },
+  ],
+  twitter: [
+    { name: "twitter:card", content: "Sylvester Das - Software programmer, Website and Application developer" },
+    { name: "twitter:site", content: "@achillies_" },
+    { name: "twitter:creator", content: "@achillies_" },
   ]
 };
