1fbb0a7f1834e09c4070426023b5bba757d69cec
Sylvester Das <sylvester.das@gmail.com>
2021-09-25 10:10:15 +0530

    preloader added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = environment.title;
+  loading = true;
 
-  constructor() {}
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500);
+  }
 }
