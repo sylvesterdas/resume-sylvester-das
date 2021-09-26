399d7db76e6b80cd344448149db8fbdeb4618e50
Sylvester Das <sylvester.das@gmail.com>
2021-09-26 14:25:49 +0530

    clear content

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-const functions = require('firebase-functions');
-const {app} = require(`${process.cwd()}/dist/server/main.js`);
-
-exports.app = functions
-  .runWith({ memory: '128MB', maxInstances: 2, minInstances: 1 })
-  .https.onRequest(app());
