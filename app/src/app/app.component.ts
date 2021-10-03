eb5af875d745f39af886c3b5ed33fcf465b169c1
Sylvester Das <sylvester.das@gmail.com>
2021-10-03 21:32:23 +0530

    preloading fix Closes #16 > Preload css links #16

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
 import { DOCUMENT } from '@angular/common';
@@ -8,18 +8,11 @@ import { DOCUMENT } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = environment.title;
-  loading = true;
   faAngleUp = faAngleUp
 
   constructor(@Inject(DOCUMENT) private document: Document) {}
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.loading = false;
-    }, 500);
-  }
 
   scrollUp(id: string) {
     (this.document.getElementById(id) as HTMLElement)
